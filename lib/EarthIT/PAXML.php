1f95681f7ebe1f778b32c3aa65451a9ee6438d0d
Dan Stevens <stevens@earthit.com>
2015-08-27 17:09:11 -0500

    PAXML::emit emits a newline after stuff.

--- lib/EarthIT/PAXML.php
+++ lib/EarthIT/PAXML.php
@@ -4,5 +4,6 @@ class EarthIT_PAXML {
 	public static function emit( $thing, $indent="", $indentDelta="\t" ) {
 		$emitter = new EarthIT_PAXML_PAXMLEmitter();
 		$emitter->emit($thing, $indent, $indentDelta, Nife_Util::getEchoFunction());
+		echo "\n"; // Usually you want this after stuff!
 	}
 }
--- test/EarthIT/PAXMLTest.php
+++ test/EarthIT/PAXMLTest.php
@@ -5,6 +5,6 @@ class EarthIT_PAXMLTest extends PHPUnit_Framework_TestCase
 	public function testEmit() {
 		ob_start();
 		EarthIT_PAXML::emit(['p', 'Hello, world!']);
-		$this->assertEquals("<p>Hello, world!</p>", ob_get_clean());
+		$this->assertEquals("<p>Hello, world!</p>\n", ob_get_clean());
 	}
 }
