2017486634b180e1b0a5f88ec7fc68add2af1f83
Dan Stevens <stevens@earthit.com>
2015-12-27 19:54:28 -0600

    s/[/array(/ in PAXMLTest.

--- test/EarthIT/PAXMLTest.php
+++ test/EarthIT/PAXMLTest.php
@@ -4,13 +4,13 @@ class EarthIT_PAXMLTest extends PHPUnit_Framework_TestCase
 {
 	public function testEmit() {
 		ob_start();
-		EarthIT_PAXML::emit(['p', 'Hello, world!']);
+		EarthIT_PAXML::emit(array('p', 'Hello, world!'));
 		$this->assertEquals("<p>Hello, world!</p>", ob_get_clean());
 	}
 
 	public function testEmitBlock() {
 		ob_start();
-		EarthIT_PAXML::emitBlock(['p', ['span', 'Hello, world!']], "\t");
+		EarthIT_PAXML::emitBlock(array('p', array('span', 'Hello, world!')), "\t");
 		$this->assertEquals(
 			"\t<p>\n".
 			"\t\t<span>Hello, world!</span>\n".
